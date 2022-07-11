6d032af5a6620a38412d39da5678cfe0ebf2415d
Wendell <wendellmorais3319@gmail.com>
2022-07-11 14:34:50 -0300

    styles: formatação do código

--- src/controllers/loginVerify.ts
+++ src/controllers/loginVerify.ts
@@ -4,6 +4,7 @@ import createAccessToken from "../services/createToken";
 
 const loginVerify = async (req: Request, res: Response) => {
     const { user, password } = req.body;
+
     const results = await UserModel.findOne({
         where: {
             user,
@@ -11,14 +12,16 @@ const loginVerify = async (req: Request, res: Response) => {
         },
         attributes: ["user", "password", "id"],
     });
+
     if (results) {
         const token = createAccessToken(user, results.id as number);
-        res.status(200).json({
+        
+        return res.status(200).json({
             message: "login successfully",
             authToken: token,
         });
-        return;
     }
+
     res.status(401).json("invalid login");
 }
 
--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 
 const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
     const authToken = req.headers.authorization;
+    
     if (!authToken) return res.status(401).json({ error: "no token provided" });
 
     jwt.verify(authToken as string, process.env.SECRET_TOKEN as string, error => {
