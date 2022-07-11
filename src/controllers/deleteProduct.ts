5d1d97342da456f03cd2d3f2c94afebb6280cfbc
Wendell <wendellmorais3319@gmail.com>
2022-07-11 15:08:21 -0300

    feat: deletar produtos

---
+++ src/controllers/deleteProduct.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express"
+import ProductModel from "../models/ProductModel";
+
+const deleteProduct = async (req: Request, res: Response) => {
+    const { id } = req.body;
+
+    if (!id) return res.status(400).json({ error: "insufficient data" });
+
+    const queryResult = await ProductModel.destroy({
+        where: { id },
+    });
+
+    if (queryResult != 1) return res.status(400).json({ error: "product not exists" });
+
+    return res.status(200).json({ message: "product deleted successfully" });
+}
+
+export default deleteProduct;
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import createUser from "./controllers/createUser";
 import createProduct from "./controllers/createProduct";
 import loginVerify from "./controllers/loginVerify";
 import getProducts from "./controllers/getProducts";
+import deleteProduct from "./controllers/deleteProduct";
 import verifyToken from "./middlewares/verifyToken";
 import { Router } from "express";
 
@@ -11,5 +12,6 @@ routes.post("/user", createUser);
 routes.post("/product", verifyToken, createProduct);
 routes.get("/products", verifyToken, getProducts);
 routes.post("/login", loginVerify);
+routes.delete("/products", verifyToken, deleteProduct);
 
 export default routes;
