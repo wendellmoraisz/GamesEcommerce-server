50483cf557811d02349794e2baf28a1d588322d9
Wendell <wendellmorais3319@gmail.com>
2022-07-11 14:36:30 -0300

    refactor: substituindo operador nulo por as

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(
-    process.env.DB_NAME ?? "",
-    process.env.DB_USER ?? "",
-    process.env.DB_PASSWORD ?? "", {
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PASSWORD as string, {
     dialect: "mysql",
     host: "localhost",
 });
