63484ae8b2ca9add9bbe69ff04fe88f147e08b01
Wendell <wendellmorais3319@gmail.com>
2022-07-13 19:40:51 -0300

    refactor: tratamento de erros no login

--- src/controllers/users/loginVerify.ts
+++ src/controllers/users/loginVerify.ts
@@ -5,24 +5,26 @@ import createAccessToken from "../../services/createToken";
 const loginVerify = async (req: Request, res: Response) => {
     const { user, password } = req.body;
 
-    const results = await UserModel.findOne({
-        where: {
-            user,
-            password,
-        },
-        attributes: ["user", "password", "id"],
-    });
+    if (!user || !password) return res.status(400).json({ error: "insufficient data" });
 
-    if (results) {
-        const token = createAccessToken(user, results.id as number);
-        
-        return res.status(200).json({
-            message: "login successfully",
-            authToken: token,
+    try {
+        const results = await UserModel.findOne({
+            where: {
+                user,
+                password,
+            },
+            attributes: ["id"],
         });
-    }
 
-    res.status(401).json("invalid login");
+        if (results) {
+            const token = createAccessToken(user, results.id as number);
+            return res.status(200).json({ message: "login successfully", authToken: token });
+        }
+        res.status(401).json({ error: "invalid login" });
+
+    } catch (e) {
+        res.status(500).json({ error: "internal server error" });
+    }
 }
 
 export default loginVerify;
